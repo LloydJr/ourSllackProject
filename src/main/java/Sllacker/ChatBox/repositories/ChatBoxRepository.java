019a138af03082157dac7a0c3d148d1214a5f4d1
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-08-25 16:09:35 -0400

    test merge

--- src/main/java/Sllacker/ChatBox/repositories/ChatBoxRepository.java
+++
@@ -1,9 +0,0 @@
-package Sllacker.ChatBox.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatBoxRepository extends JpaRepository{
-    
-}
