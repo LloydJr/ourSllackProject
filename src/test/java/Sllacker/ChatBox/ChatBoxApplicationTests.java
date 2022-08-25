0c1e08f2d59ddb89e2ac76483a3cded0eee57a78
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-08-25 14:16:04 -0400

    initial commit

---
+++ src/test/java/Sllacker/ChatBox/ChatBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package Sllacker.ChatBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
