5f1357a8b9029adbe940cbbc5513ecc3cb2a4430
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-08-28 15:20:08 -0400

    User updates

--- src/main/java/Sllacker/ChatBox/models/User.java
+++ src/main/java/Sllacker/ChatBox/models/User.java
@@ -1,6 +1,7 @@
 package Sllacker.ChatBox.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import javax.persistence.*;
@@ -22,10 +23,11 @@ public class User {
    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
    private List<Message> messages = new ArrayList<>();
 
-   @ManyToOne
-   @JoinColumn(name = "channel")
-   @JsonBackReference
-   private Channel channel;
+   @JoinTable(name = "channels",
+           joinColumns = {@JoinColumn(name = "user_id")},
+           inverseJoinColumns = {@JoinColumn(name = "channel_id")}
+   )
+   private List<Channel> channels = new ArrayList<>();
 
    @JsonManagedReference
    public List<Message> getMessages() {
@@ -64,8 +66,11 @@ public class User {
       this.password = password;
    }
 
-   public Channel getChannel() {return channel;}
+   public List<Channel> getChannels() {
+      return channels;
+   }
 
-   public void setChannel(Channel channel) {this.channel = channel;}
-
+   public void setChannels(List<Channel> channels) {
+      this.channels = channels;
+   }
 }
