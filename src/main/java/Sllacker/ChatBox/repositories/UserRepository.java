6c3d3b160c607c9be67c47bfd8c2058df0ac2a86
tebarker <tenaebarker@gmail.com>
2022-08-29 14:49:13 -0400

    added userdetails and sllacker user details, added methods to user repo, and created a userdetails service file

--- src/main/java/Sllacker/ChatBox/repositories/UserRepository.java
+++ src/main/java/Sllacker/ChatBox/repositories/UserRepository.java
@@ -2,11 +2,14 @@ package Sllacker.ChatBox.repositories;
 
 import Sllacker.ChatBox.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
+@Query("SELECT u FROM User u WHERE u.userName = ?1")
+User findByUsername(String userName);
 
-
-    
 }
