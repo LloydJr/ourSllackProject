b09ba19f4ed92e4cd8ff2ff39559e83546c9c44c
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-09-02 16:20:44 -0400

    Added a import to index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from '.serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
