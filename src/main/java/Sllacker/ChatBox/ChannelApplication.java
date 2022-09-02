defb57837e447c974f29019714e115c16184606f
tebarker <tenaebarker@gmail.com>
2022-09-02 16:28:41 -0400

    channel app and cross origins

--- src/main/java/Sllacker/ChatBox/ChannelApplication.java
+++ src/main/java/Sllacker/ChatBox/ChannelApplication.java
@@ -1,13 +1,24 @@
 package Sllacker.ChatBox;
 
+import Sllacker.ChatBox.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChannelApplication {
+public class ChannelApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChannelApplication.class, args);
 	}
 
+	@Autowired
+	private UserRepository userRepository;
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
