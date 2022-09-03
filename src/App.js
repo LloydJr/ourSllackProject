a3a5dc427d7672b46e892797ee2004894ada7dc8
LloydJr <98184266+LloydJr@users.noreply.github.com>
2022-09-03 16:41:02 -0400

    Fix imports

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Sidebar from "./components/Sidebar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MuiAccordion from "./components/MuiAccordion";
 import ChatRoom from "./components/ChatRoom";
-import ChannelPage from "./components/ChannelPage";
+import ChannelPage from "./components/ChannelComp/ChannelPage";
 import {container} from "./App.css"
 import Box from '@mui/material/Box';
 import Footer from "./components/Footer";
@@ -16,6 +16,7 @@ import App1 from './components/App1'
 import DirectMessage from "./components/Pages/DirectMessage"
 import Channles from "./components/Pages/Channles";
 import UserProfile from './components/Pages/UserProfile';
+import Register from './components/Pages/Register'
 
 
 function App() {
@@ -39,7 +40,8 @@ function App() {
         <div></div>
         <br></br>
         <Routes>
-          <Route exact path="/" element={<App1 />}/>
+          <Route path="/" element={<Register />} />
+          <Route path="/sign_in" element={<App1 />}/>
           <Route path="/user_profile" element={<UserProfile/>} />
           <Route path="/about" element={<About />} />
           <Route path="/direct_messages" element={<DirectMessage />} />
