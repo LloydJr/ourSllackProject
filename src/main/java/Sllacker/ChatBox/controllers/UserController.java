c8bdf963a04e277e5fa628c335c8b81ed039934b
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-09-04 21:45:56 -0400

    added Channel repo Queries

--- src/main/java/Sllacker/ChatBox/controllers/UserController.java
+++ src/main/java/Sllacker/ChatBox/controllers/UserController.java
@@ -1,13 +1,19 @@
 package Sllacker.ChatBox.controllers;
 
 
+import Sllacker.ChatBox.models.Channel;
 import Sllacker.ChatBox.models.User;
+import Sllacker.ChatBox.repositories.ChannelRepository;
+import Sllacker.ChatBox.repositories.MessageRepository;
 import Sllacker.ChatBox.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import Sllacker.ChatBox.controllers.ChannelController;
+
+
 
 import java.util.List;
 
@@ -16,14 +22,31 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-  private UserRepository repository;
 
-  public UserController (UserRepository repository) {this.repository = repository;}
+
+
+  private UserRepository repository;
+  private ChannelRepository channelRepository;
+
+
+@Autowired
+  public UserController (UserRepository repository, ChannelRepository channelRepository)
+    {this.repository = repository; this.channelRepository = channelRepository;}
+
 
     @PostMapping
-    public @ResponseBody ResponseEntity <User> postUser(@RequestBody User user){
-      user = repository.save(user);
+    public @ResponseBody ResponseEntity <User> postUser(@RequestBody User user, Channel channel){
+
+//        for (int i = 0; i < channelRepository.findAll().size(); i++) {
+//            if (String.valueOf("The Lounge").equalsIgnoreCase(channelRepository.findAll().get(i).getChannelName())) {
+//                channel = channelRepository.findAll().get(i);
+//            }
+//        }
+        channel = channelRepository.findByChannelName("The Lounge");
+        channel.getChannel_users().add(user);
+        user.getChannels().add(channel);
+        repository.save(user);
+        channelRepository.save(channel);
       return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 
