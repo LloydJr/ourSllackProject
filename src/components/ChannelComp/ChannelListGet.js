eaaa8a63a891eb15ebb7745167f8097c45042a4c
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-09-06 11:09:18 -0400

    added more thought into the code

--- src/components/ChannelComp/ChannelListGet.js
+++ src/components/ChannelComp/ChannelListGet.js
@@ -8,53 +8,58 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 function ChannelPage() {
 
+
+
     const [data, setData] = useState([])
     const [user, setUser] = useState([])
 
-    useEffect( () => {
+    
+    useEffect(  () => {
         const logChannel = localStorage.getItem("channelName")
         axios.get(`http://localhost:8080/channel/messages/${logChannel}/`)
        .then(res => {
            setData(res.data)
-           setUser(res.data)
-           window.scrollTo(0, document.body.scrollHeight)
-
        }).catch(err => console.log(err))
-    }, [])
+       
+    }, [data, data])
 
 
-   
+    useEffect( () => {
+      const logChannel = localStorage.getItem("channelName")
+      axios.get(`http://localhost:8080/channel/messages/${logChannel}/`)
+     .then(res => {
+         setUser(res.data)
+     }).catch(err => console.log(err))
+    }, [user])
+
     const headName = user.map((data) => {
         return (
             <h1 className="box3 text-3xl text-white text-center">{data.channel.channelName}</h1>
-        )
-
-        
+        ) 
     })
 
+   
 
-    const arr = data.map((data, index) => {
-        return (
-           
-            <div className="box2 text-center py-8 px-8 mx-auto rounded-xl sm:py-4">
-            <div class="text-center space-y-2 sm:text-left">
-              <div class="space-y-0.5">
+    // const arr = data.map((data1, index) => {
+    //     return (
+    //         <div className="box2 text-center py-8 px-8 mx-auto rounded-xl sm:py-4">
+    //         <div class="text-center space-y-2 sm:text-left">
+    //           <div class="space-y-0.5">
                 
-                <p class="text-slate-500 font-medium ">
-                {data.message}      </p>
-                <p class="text-lg text-white font-semibold">
-                    < AccountCircleIcon/>
-                {data.userName}
-                </p>
-              </div>
-            </div>
-          </div>
-          
-            
-        )
-    })
+    //             <p class="text-slate-500 font-medium ">
+    //             <AccountCircleIcon/>
+    //             {data1.userName}
+    //                  </p>
+    //             <p class="text-lg text-white font-semibold">
+    //             {data1.message} 
+    //             </p>
+    //           </div>
+    //         </div>
+    //       </div>   
+    //     )
+    // })
 
-
+    
 
     return (
         <div>
@@ -71,8 +76,27 @@ function ChannelPage() {
            justifyConent: 'center',
        }}>
        <tr>
-        </tr>
-       {arr}
+        </tr> 
+        {data.map((data1) => { 
+           const arr = (
+        <div className="box2 text-center py-8 px-8 mx-auto rounded-xl sm:py-4">
+            <div class="text-center space-y-2 sm:text-left">
+              <div class="space-y-0.5">
+                
+                
+                <AccountCircleIcon/>
+                {data1.userName}
+            
+                <p class="text-lg text-white font-semibold">
+                {data1.message} 
+                </p>
+              </div>
+            </div>
+          </div>   
+           )
+           return arr;
+        })}
+       {/* {arr} */}
 
        </div >
        <div className='min-w-full'>
