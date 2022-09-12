975017034bd2e142cba32bf83d1494391c8f4e9c
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-09-12 11:45:16 -0400

    cleaning up the code

--- src/main/java/Sllacker/ChatBox/repositories/UserRepository.java
+++ src/main/java/Sllacker/ChatBox/repositories/UserRepository.java
@@ -2,14 +2,13 @@ package Sllacker.ChatBox.repositories;
 
 import Sllacker.ChatBox.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-@Query("SELECT u FROM User u WHERE u.userName = ?1")
-User findByUsername(String userName);
+// @Query("SELECT u FROM User u WHERE u.userName = ?1")
+User findByUserName(String userName);
+
 
 }
